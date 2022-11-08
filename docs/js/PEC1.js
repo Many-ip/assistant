d414e030bff8fa190e81554f289f9b75b7499553
Manel <57791064+Many-ip@users.noreply.github.com>
2022-11-08 23:18:36 +0100

    Add files via upload

--- docs/js/PEC1.js
+++ docs/js/PEC1.js
@@ -21,6 +21,9 @@ function sumaVector() {
     }
     return suma
 }
+function media() {
+    return sumaVector()/stringToArrayNumber().length
+}
 
 function mediana() {
     let arr=stringToArrayNumber()
@@ -51,32 +54,48 @@ function Q3() {
     let q3=0
     if(n%2==0) q3=arr.slice(mitad,n)
     else q3=arr.slice(mitad+1,n)
-    console.log(q3)
     if(q3.length%2==0){
         let q3Mitad=(q3.length)/2
         return  (q3[q3Mitad]+q3[q3Mitad-1])/2
     }else return q3[parseInt(q3.length/2)]
 }
+function desviacionEstandarMuestral(){
+    let arr=stringToArrayNumber()
+    let sum=0
+    for (let i = 0; i < arr.length; i++) {
+        sum+=Math.pow((arr[i]-media()),2)
+    }
+    return Math.sqrt(sum/(arr.length-1))
+}
+function desviacionEstandarPoblacional(){
+    let arr=stringToArrayNumber()
+    return desviacionEstandarMuestral()*Math.sqrt(arr.length-1)/Math.sqrt(arr.length)
+}
 function solve() {
     document.getElementById("Solution").innerHTML=
     `<div class="container">
-        <h1 class="mt-5">Ordenar</h1>
+        <h5 class="mt-5">Ordenar</h5>
         <p class="lead">Ordenamos el vector para mayor comodidad para los siguientes apartados</p>
         <p>[${stringToArrayNumber()}]</p>
 
-        <h1 class="mt-5">Media/Promedio</h1>
+        <h5 class="mt-5">Media/Promedio</h5>
         <p class="lead">Sumamos todos los numeros del vector y se divide por su longitud</p>
-        <p>${sumaVector()}/${stringToArrayNumber().length}=${sumaVector()/stringToArrayNumber().length}</p>
+        <p>${sumaVector()}/${stringToArrayNumber().length}=${media()}</p>
     
-        <h1 class="mt-5">Mediana</h1>
+        <h5 class="mt-5">Mediana</h5>
         <p class="lead">En caso que la logitud sea inpar es escoger justamente la que esta en medio, en caso de ser par, se ha de hacer una media entre los dos valores que esten en el medio</p>
         <p>${mediana()}</p>
         
-        <h1 class="mt-5">Q1</h1>
+        <h5 class="mt-5">Q1</h5>
         <p>${Q1()}</p>
 
-        <h1 class="mt-5">Q3</h1>
+        <h5 class="mt-5">Q3</h5>
         <p>${Q3()}</p>
+
+        <h5 class="mt-5">Desviación estandar muestral</h5>
+        <p>${desviacionEstandarMuestral()}</p>
+        <h5 class="mt-5">Desviación estandar poblacional</h5>
+        <p>${desviacionEstandarPoblacional()}</p>
      </div>`
 }
 
